6002e106d60d0f292a34107b3b3649dde47e0fbd
kaiyidai <xhqmerida@gmail.com>
2023-02-26 11:43:56 -0500

    first

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Calendar  from './components/Calendar'
+
+const App = ()=>{
+  return (
+    <div className='App' >
+<h1>Kaiyi's Project</h1>
+<h2>not sure</h2>
+<Calendar/>
+    </div>
+  )
+}
+
+export default App
